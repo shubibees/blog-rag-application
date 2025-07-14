18e8d50d8de00494227f75843b74583e4620caef
Shubham Kumar <Shubham@svrind.local>
2025-07-14 14:31:53 +0530

    add corse in main

--- main.py
+++ main.py
@@ -3,10 +3,20 @@ from routers import search_router
 from dotenv import load_dotenv
 import uvicorn
 import os
+from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv()
 
 app = FastAPI()
+
+# Add CORS middleware to allow all origins, methods, and headers
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(search_router.router)
 if __name__ == "__main__":
